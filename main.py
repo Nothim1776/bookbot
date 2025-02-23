eaaa1e1b8430f1eb8edaf0d95a174e4eb4c50701
Nothim1776 <wgs3kshun8@DESKTOP-AHMGHDL.>
2025-02-23 11:06:50 -0600

    C: Refactored

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import word_count
+
 def get_book_text(path):
 	with open(path) as f:
 		file_contents = f.read()
@@ -8,13 +10,6 @@ def pull_book():
 	text = get_book_text(path)
 	return(text)
 
-def word_count():
-	text = pull_book()
-	words = text.split()
-	num_words = 0
-	for word in words:
-		num_words += 1
-	print(f"{num_words} words found in the document")
-
-
-word_count()
+text = pull_book()
+num_words = word_count(text)
+print(f"{num_words} words found in the document")
---
+++ stats.py
@@ -0,0 +1,6 @@
+def word_count(text):
+        words = text.split()
+        num_words = 0
+        for word in words:
+                num_words += 1
+        return num_words
